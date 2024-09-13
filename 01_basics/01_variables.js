e471445ab681d1e1b4eb05061fb72ac402fc18fe
janvidashlaniya <janvidashlaniya@gmail.com>
2024-09-13 11:30:50 +0530

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "janvidashlaniya.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2  
+// not allowed
+
+accountEmail = "hcc@hc.com"
+accountPassword = "2121121211"
+accountCity = "Bengluru"
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+    let name = "janvi"  //( string data type)   
+    let age  = 34  //(number)
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => (abhi value asigend nahi hue hai)
+    // symbol => unique
+
+    // object
+
+    console.log(typeof "janvi");
