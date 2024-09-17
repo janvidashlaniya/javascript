0295c4aee93435d79a20a96bbc4f6cb0dac29a3b
janvidashlaniya <janvidashlaniya@gmail.com>
2024-09-17 11:09:54 +0530

    ConversionOperation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,20 @@
+// let score = 45
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof  valueInNumber);  
+// console.log(valueInNumber);
+
+
+    // "33" => 33
+    // "33abc" => NaN (note an number)
+    // true => 1; false => 0
+
+    let isLoggedIn = 1
+
+    let booleanIsLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanLoggedIn);
+
+
